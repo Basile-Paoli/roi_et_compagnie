81761d0fb471b61d912ab87d64012f1834257261
Basile Paoli <basile.paoli@gmail.com>
2025-07-20 14:50:07 +0200

    fix: locations and hypnotizer

--- game/gamestate.py
+++ game/gamestate.py
@@ -1,6 +1,6 @@
 from typing import Optional, Iterable
 from game.card_types import KingdomCard, Location, Penalty, get_initial_locations, \
-    initial_penalties
+    initial_penalties, LocationType
 from game.dice import get_dice, Die
 from game.inhabitant import Inhabitant, Elf, Dragon, Sorcerer, Hypnotizer, \
     initial_inhabitants, inhabitant_from_json
@@ -78,7 +78,7 @@ class DieRollState:
         state.dice = [Die.from_json(die) for die in data["dice"]]
         state.nb_tries = data["nb_tries"]
         return state
-    
+
     def __str__(self) -> str:
         dice_str = ", ".join(str(die) for die in self.dice)
         return f"DieRollState(nb_tries={self.nb_tries}, dice=[{dice_str}])"
@@ -91,7 +91,7 @@ class ShopSlot:
         self.locations = locations
         self.inhabitant = inhabitant
         self.type = locations[0].location_type
-    
+
     def to_json(self) -> dict:
         return {
             "locations": self.locations,
@@ -105,6 +105,9 @@ class ShopSlot:
         inhabitant = inhabitant_from_json(data["inhabitant"]) if data["inhabitant"] else None
         return ShopSlot(locations, inhabitant)
 
+
+class TargetNeededForDragonException(Exception):
+    pass
 
 class Game:
 
@@ -184,25 +187,26 @@ class Game:
         else:
             return player.id == self.current_player.id
 
-    def take_inhabitant(self, inhabitant: Inhabitant, target_player: Player) -> None:
+    def take_inhabitant(self, inhabitant: Inhabitant, target_player: Player, end_turn = True) -> None:
         slot_with_inhabitant = next((slot for slot in self.shop if slot.inhabitant == inhabitant), None)
         if not slot_with_inhabitant:
             raise ValueError("Inhabitant not found in shop.")
-        slot_with_inhabitant.inhabitant = None
 
         location_to_take = self.matching_location(inhabitant)
         if location_to_take:
             self.take_location(location_to_take)
 
+        slot_with_inhabitant.inhabitant = None
+
         target_player.kingdom.append(inhabitant)
 
         if isinstance(inhabitant, Hypnotizer):
-            self.resolve_hypnotizer()
+            self.resolve_hypnotizer(slot_with_inhabitant.type)
 
         self.fill_shop()
         if isinstance(inhabitant, Sorcerer):
             self.die_roll = DieRollState()
-        else:
+        elif end_turn:
             self.next_player()
     def take_penalty(self, target_player: Player) -> None:
         if not self.penalty_deck:
@@ -210,7 +214,7 @@ class Game:
         penalty = self.penalty_deck.pop()
         target_player.kingdom.append(penalty)
 
-    def matching_location(self, inhabitant) -> Optional[Location]:
+    def matching_location(self, inhabitant: Inhabitant) -> Optional[Location]:
         slot = next((loc for loc in self.shop if loc.type == inhabitant.related_location), None)
         if slot and inhabitant == slot.inhabitant:
             return slot.locations[-1]
@@ -223,15 +227,17 @@ class Game:
                 self.current_player.kingdom.append(location)
                 break
 
-    def resolve_hypnotizer(self) -> None:
+    def resolve_hypnotizer(self, hypnotizer_slot: LocationType) -> None:
         for i, slot in enumerate(self.shop):
             if i == len(self.shop) - 1:
                 return
-            if isinstance(slot.inhabitant, Hypnotizer):
+            if slot.type == hypnotizer_slot:
                 next_slot = self.shop[i + 1]
                 if not next_slot.inhabitant:
                     continue
-                self.take_inhabitant(next_slot.inhabitant, self.current_player)
+                if isinstance(next_slot.inhabitant, Dragon) :
+                    raise TargetNeededForDragonException()
+                self.take_inhabitant(next_slot.inhabitant, self.current_player, end_turn=False)
 
     def fill_shop(self) -> None:
         while any(slot.inhabitant is None for slot in self.shop):
