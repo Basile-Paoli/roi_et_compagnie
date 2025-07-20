d5a55a5c30fdef93293d387f8511dc1cabbb4c6b
Spatulox <M78stormtrooper@laposte.net>
2025-07-20 14:01:55 +0200

    add debug things to main

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from game.gamestate import Game
 from interface.menu import show_menu
 from interface.game_loop import game_loop
+import os
+
+
+def print_inhabitant_images(initial_inhabitants):
+    for inhabitant in initial_inhabitants:
+        path = inhabitant.image_path
+        exists = os.path.exists(path)
+        print(f"{path} - {'OK' if exists else 'MANQUANTE'}")
 
 def main():
+    
     nb_joueurs = show_menu()
     state = Game(nb_joueurs)
+    print_inhabitant_images(state.inhabitant_deck)
     game_loop(state)
 
 if __name__ == "__main__":
