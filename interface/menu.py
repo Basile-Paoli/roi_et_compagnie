eba07487fc8868330231b0b7bacc973d14e26c77
Spatulox <M78stormtrooper@laposte.net>
2025-07-20 16:50:21 +0200

    rework menu.py to be able to load a save

--- interface/menu.py
+++ interface/menu.py
@@ -1,10 +1,20 @@
 import pygame
+import os
 
-def show_menu():
+def menu() -> int:
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     font = pygame.font.SysFont(None, 48)
+
     options = [2, 3, 4]
+    has_save = os.path.isfile("gamestate.json")
+    menu_options: list[tuple[str, int]] = []
+
+    if has_save:
+        menu_options.append(("Reprendre la partie", -1))
+    for nb in options:
+        menu_options.append((f"{nb} joueur{'s' if nb > 1 else ''}", nb))
+
     selected = 0
     running = True
 
@@ -13,9 +23,9 @@ def show_menu():
         title = font.render("Sélection du nombre de joueurs", True, (255, 255, 0))
         screen.blit(title, (100, 100))
 
-        for i, nb in enumerate(options):
+        for i, (txt, _) in enumerate(menu_options):
             color = (255, 255, 0) if i == selected else (200, 200, 200)
-            text = font.render(f"{nb} joueur{'s' if nb > 1 else ''}", True, color)
+            text = font.render(txt, True, color)
             screen.blit(text, (150, 200 + i * 60))
 
         pygame.display.flip()
@@ -26,9 +36,11 @@ def show_menu():
                 exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    selected = (selected - 1) % len(options)
+                    selected = (selected - 1) % len(menu_options)
                 elif event.key == pygame.K_DOWN:
-                    selected = (selected + 1) % len(options)
+                    selected = (selected + 1) % len(menu_options)
                 elif event.key == pygame.K_RETURN:
                     pygame.quit()
-                    return options[selected]
+                    return menu_options[selected][1]
+
+    return -2
