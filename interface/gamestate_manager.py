8e1b68dc28f84087456ec44c6eed1b47c5e090da
Spatulox <M78stormtrooper@laposte.net>
2025-07-20 16:28:11 +0200

    add gamestate_manager

---
+++ interface/gamestate_manager.py
@@ -0,0 +1,11 @@
+import json
+from typing import Any
+
+def save_gamestate(state_json: dict, filename: str = "gamestate.json"):
+    with open(filename, "w", encoding="utf-8") as f:
+        #json.dump(state_json, f, default= lambda o: o.to_json())
+        json.dump(state_json, f, indent=4, default= lambda o: o.to_json())
+
+def load_gamestate(filename: str = "gamestate.json") -> dict[str, Any]:
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
